7684d87eb1b8e469f4c5256d2a8d2e2e83d26caa
Hasibul Hoque Probal <hoqueefrom@gmail.com>
2026-04-17 12:08:47 +0600

    display lesson buttons dynamically

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 // API call to fetch all lessons
-function fetchLessons(){
-    const url = 'https://openapi.programming-hero.com/api/levels/all';
-    fetch(url)
-    .then(res => res.json())
-    .then(json => showLessons(json.data));
+function fetchLessons() {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => showLessons(json.data));
+}
+
+// Render lesson buttons
+const lessonsCont = document.getElementById("lessons-container");
+
+function showLessons(lessons) {
+  lessons.forEach((lesson) => {
+    const button = document.createElement("button");
+    button.setAttribute("onclick", `fetchWordCards(${lesson.level_no})`);
+    button.innerHTML = `<i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}`;
+    button.classList.add(
+      "btn",
+      "btn-outline",
+      "btn-primary",
+      "lessons-btn",
+      `lesson-btn-${lesson.level_no}`,
+    );
+    lessonsCont.appendChild(button);
+  });
 }
 
 fetchLessons();
